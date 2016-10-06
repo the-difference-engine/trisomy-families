c9351db07f1518538fc96e6109e849187086cd9d
Jack Shales <jackshalesmusic@gmail.com>
2016-10-05 19:13:51 -0500

    fixed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
-=======
-  get '/' => 'pages#index'
-
->>>>>>> 7e078aa92241530d5308fb42fb6bcbdc0c9f6229
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'users#index'
-
+  root 'users#index'
 end
