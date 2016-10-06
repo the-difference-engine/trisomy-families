0a70aeb16d291d2d72751b22c6cedacc2a947805
matvacha <matvacha@yahoo.com>
2016-10-05 21:03:01 -0500

    update form and routes

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,2 +1,19 @@
 class ChildrenController < ApplicationController
+  def new_profile
+  end
+
+  def create_profile
+    @child = Child.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      trisomy_type: params[:trisomy_type],
+      birth_date: params[:date_of_birth],
+      death_date: params[:date_of_death],
+      state: params[:state],
+      city: params[:city],
+      trisomy_story: params[:trisomy_story]
+    )
+    @child.save
+    redirect_to '/'
+  end
 end
