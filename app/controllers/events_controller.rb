c1b68b69c8492668b47c32b71a9d8152ca155586
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-06 20:50:57 -0500

    Added skip verify auth token to use current_user.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,17 @@
 class EventsController < ApplicationController
-# before_action :authenticate_user!
+
+# Needs to be here for current_user to work
+# Might not be the most secure option
+skip_before_filter :verify_authenticity_token
 
   def create
-    @user = User.find_by(id: 1)
+    @user = User.find_by(id: current_user.id)
+
     @event = Event.new(title: params[:title],
                        description: params[:description],
                        date: Date.parse(params[:date_time]))
     @event.save
+
     redirect_to '/'
   end
 
