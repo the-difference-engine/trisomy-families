688cbdc524bedc869d6e734a8cb59524e1c30f51
matvacha <matvacha@yahoo.com>
2016-10-06 21:00:58 -0500

    update to add pictures

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -3,12 +3,15 @@ class ChildrenController < ApplicationController
   end
 
   def create_profile
+    calculated_birth_date = params[:date_of_birth].blank? ? nil : Date.parse(params[:date_of_birth])
+    calculated_death_date = params[:date_of_death].blank? ? nil : Date.parse(params[:date_of_death])
+
     @child = Child.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       trisomy_type: params[:trisomy_type],
-      birth_date: Date.parse(params[:date_of_birth]),
-      death_date: Date.parse(params[:date_of_death]),
+      birth_date: calculated_birth_date,
+      death_date: calculated_death_date,
       state: params[:state],
       city: params[:city],
       trisomy_story: params[:trisomy_story],
