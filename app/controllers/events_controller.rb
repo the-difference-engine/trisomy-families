d7721b293a46e1700df117e3f62e87d2dc714056
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-06 20:31:57 -0500

    Create event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,13 @@
 class EventsController < ApplicationController
+# before_action :authenticate_user!
 
   def create
+    @user = User.find_by(id: 1)
+    @event = Event.new(title: params[:title],
+                       description: params[:description],
+                       date: Date.parse(params[:date_time]))
+    @event.save
+    redirect_to '/'
   end
 
 end
