610cf302a4987a6938929321e5986d29e457f511
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-08 13:39:16 -0500

    Tests for Event model.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Saved to database' do
+    Event.create({:title => 'Event Title', :description => 'Event Descriptions', :date => '2016-10-06'})
+
+    expect(Event.all.count).to eq(1)
+  end
+  it 'Contains all field data' do
+    event = Event.create({:title => 'Event Title', :description => 'Event Descriptions', :date => '2016-10-06'})
+
+    expect(event.title).to eq('Event Title')
+    expect(event.description).to eq('Event Descriptions')
+    expect(event.date.to_s).to eq('2016-10-06 00:00:00 UTC')
+  end
 end
