911df1a5d0784fa6f3809d25d3cce3a17d1501db
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-08 01:13:12 -0500

    Refactor event params.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,12 +7,21 @@ skip_before_action :verify_authenticity_token
   def create
     @user = User.find_by(id: 1)
 
-    @event = Event.new(title: params[:title],
-                       description: params[:description],
-                       date: Date.parse(params[:date_time]))
+    @event = Event.new(event_params)
+    @event.date = parse_date
     @event.save
 
     redirect_to '/'
   end
+
+  private
+
+    def event_params
+      params.permit(:title, :description, :date)
+    end
+
+    def parse_date
+      return Date.parse(params[:date_time])
+    end
 
 end
