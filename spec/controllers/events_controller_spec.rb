9c5b5be86e99163e2f73949ed26aecf030f19982
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-09 22:28:53 -0500

    Fix date key hash

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
   describe 'POST #create' do
-    subject { post :create, { :title => 'Event Title', :description => 'Event Descriptions', :date_time => '2016-10-16' } }
+    subject { post :create, { :event => { :title => 'Event Title', :description => 'Event Descriptions', :date => '2016-10-16' } } }
 
     it 'redirects to homepage' do
-      expect(subject).to redirect_to("/")
+      expect(subject).to redirect_to('/')
     end
   end
 end
