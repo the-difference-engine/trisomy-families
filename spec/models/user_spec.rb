ff7fa7b399fae428731a3116ad8fcc339a06b7e2
Jack Shales <jackshalesmusic@gmail.com>
2016-10-19 18:44:01 -0500

    add user_id to children

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#children' do
+    it 'should yield all children belonging to the user' do
+    user = User.new
+    end
+  end
 end
