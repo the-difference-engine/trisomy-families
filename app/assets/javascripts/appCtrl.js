b1f344712fc126097e0606964a68241428bf0206
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-19 18:31:53 -0500

    Modify events table

--- app/assets/javascripts/appCtrl.js
+++ app/assets/javascripts/appCtrl.js
@@ -5,7 +5,7 @@
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    $scope.events = [{title: 'All Day Event',start: new Date(y, m, 1)}];
+    $scope.events = [{title: 'New Event',start: new Date(y, m, 1)}];
 
     $scope.eventSources = [$scope.events];
   });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007004026) do
+ActiveRecord::Schema.define(version: 20161019232450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,13 @@ ActiveRecord::Schema.define(version: 20161007004026) do
   create_table "events", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.string   "type"
+    t.boolean  "allDay"
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|
