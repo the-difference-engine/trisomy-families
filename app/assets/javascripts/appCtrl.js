c352578731a64c8e2c88e3981f0d8e0f436fd802
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-19 18:13:37 -0500

    Commit

--- app/assets/javascripts/appCtrl.js
+++ app/assets/javascripts/appCtrl.js
@@ -1,6 +1,12 @@
 /* angular */
 (function() {
   angular.module('app').controller('calendarCtrl', function($scope) {
-    $scope.eventSources = [];
+    var date = new Date();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    $scope.events = [{title: 'All Day Event',start: new Date(y, m, 1)}];
+
+    $scope.eventSources = [$scope.events];
   });
 })();
