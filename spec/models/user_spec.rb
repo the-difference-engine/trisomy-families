58f06d48d8a841e4899fe132d9e783e2fd675241
Jack Shales <jackshalesmusic@gmail.com>
2016-10-19 19:18:32 -0500

    add logo to header

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#children' do
-    it 'should yield all children belonging to the user' do
-    user = User.new
-    end
-  end
+  
 end
