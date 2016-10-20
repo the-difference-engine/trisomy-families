02d84818314c0b60fa40e31e9a2f49fa6661f5a2
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-19 20:57:44 -0500

    Show events on calendar

--- app/assets/javascripts/appCtrl.js
+++ app/assets/javascripts/appCtrl.js
@@ -1,13 +1,24 @@
 /* angular */
-(function() {
-  angular.module('app').controller('calendarCtrl', function($scope) {
+(function(){
+  angular.module('app').controller('calendarCtrl', function($scope, $http) {
     var date = new Date();
+    var d = date.getDate();
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    $scope.events = [{title: 'New Event',start: new Date(y, m, 1)}];
+    $scope.events = [];
+
+    $scope.setUp = function() {
+      $http.get('/api/v1/events').then(function(response) {
+        console.log(JSON.stringify(response.data));
+        for(var i = 0; i < response.data.length; i++) {
+          $scope.events.push(response.data[i]);
+        }
+      })
+    };
 
 
     $scope.eventSources = [$scope.events];
-  });
+    window.$scope = $scope;
+  })
 })();
--- app/views/api/v1/events/index.json.jbuilder
+++ app/views/api/v1/events/index.json.jbuilder
@@ -1,3 +1,5 @@
 json.array!(@events) do |event|
-  json.extract! event, :id, :title, :description, :start_time, :end_time
+  json.extract! event, :title, :description
+  json.start event.start_time
+  json.end event.end_time
 end
