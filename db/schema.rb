49ecf1510561d606daaefd39dc28c41c1c20226f
Ramiro Trejo <trejoramiro1@gmail.com>
2016-10-20 19:33:53 -0500

    Rename columns in events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019234919) do
+ActiveRecord::Schema.define(version: 20161021003213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20161019234919) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.datetime "start_time"
-    t.datetime "end_time"
+    t.datetime "start"
+    t.datetime "end"
     t.string   "event_type"
     t.boolean  "allDay"
     t.string   "location"
