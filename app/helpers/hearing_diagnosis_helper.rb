3b38946d2ac68614dc88935dbe4e543b8659fe3c
matvacha <matvacha@yahoo.com>
2016-11-03 20:26:20 -0500

    add neccessary helpers

---
+++ app/helpers/hearing_diagnosis_helper.rb
@@ -0,0 +1,11 @@
+module HearingDiagnosisHelper
+  def hearing_diagnosis
+    [
+      ['None'],
+      ['Left'],
+      ['Right'],
+      ['Both'],
+      ['Unsure']  
+    ]
+  end
+end
---
+++ app/helpers/primary_trisomy_diagnosis_helper.rb
@@ -0,0 +1,12 @@
+module PrimaryTrisomyDiagnosisHelper
+  def primary_trisomy_diagnosis
+    [
+      ['Full'],
+      ["Partial"],
+      ['Mosaic'],
+      ['Balanced Translocation'],
+      ['Unbalanced Translocation'],
+      ['Ring'] 
+    ]
+  end
+end
