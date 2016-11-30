aa42ba9a8c0fc8cf8e9f24259abb688ccb96b6ce
Ramiro Trejo <trejoramiro1@gmail.com>
2016-11-29 21:05:13 -0600

    Create physician table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129005814) do
+ActiveRecord::Schema.define(version: 20161130025737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -446,6 +446,18 @@ ActiveRecord::Schema.define(version: 20161129005814) do
     t.boolean  "unsure"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
+    t.string "website"
+    t.string "speciality"
   end
 
   create_table "primary_diagnoses", force: :cascade do |t|
