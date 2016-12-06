1aef2f96051038b802d7328b8070fbdc09b8b5e9
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-05 20:01:16 -0600

    Add params hash

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,3 +1,27 @@
 class Api::V1::SearchController < ApplicationController
+
+  def index
+    if params[:type] == "physician"
+      rows = Physician.where(hash_params)
+
+    elsif params[:type] == "family"
+      rows = User.where(hash_params)
+
+    else
+      render nothing: true, status: 404
+    end
+
+  end
+
+  private
+
+  def hash_params
+    h = {}
+    h[:state] = params[:state] if params[:state] != ""
+    h[:city] = params[:city] if params[:city] != ""
+    h[:last_name] = params[:last_name] if params[:last_name] != ""
+    h[:speciality] = params[:speciality] if params[:speciality] != "" && params[type] == "physician"
+    return h
+  end
   
 end
