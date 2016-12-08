84223574ee23f257a9c4cb7c4797fccad5e53382
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-08 13:18:44 -0600

    Edit family serializer

--- app/serializers/api/v1/family_serializer.rb
+++ app/serializers/api/v1/family_serializer.rb
@@ -2,7 +2,7 @@ class Api::V1::FamilySerializer < ActiveModel::Serializer
   type 'family'
   attributes :id, :last_name, :address, :city, :state, :zip_code
   attribute :latitude do
-    object.latitude.to_f
+      object.latitude.to_f
   end
   attribute :longitude do
     object.longitude.to_f
