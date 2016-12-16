f95eb39a66fad18a64bfe675d619bf5dae794030
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-15 19:11:09 -0600

    Add to config production file for AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,17 +2,17 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
+  config.assets.raise_runtime_errors = true
+
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
@@ -61,7 +63,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -70,9 +72,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
+  config.assets.debug = true
+
   config.action_mailer.perform_deliveries = true
 
+  config.active_record.migration_error = :page_load
+
   config.action_mailer.default :charset => "utf-8"
+
+  config.active_support.deprecation = :log
+
 
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
@@ -83,6 +92,18 @@ Rails.application.configure do
       user_name: ENV["GMAIL_USERNAME"],
       password: ENV["GMAIL_PASSWORD"]
    }
+
+# environment variables are still missing, need to add them at a later date.
+
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch(ENV['S3_BUCKET_NAME']),
+    access_key_id: ENV.fetch(ENV['AWS_ACCESS_KEY_ID']),
+    secret_access_key: ENV.fetch(ENV['AWS_SECRET_ACCESS_KEY']),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
