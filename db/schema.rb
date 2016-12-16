fec5cce5eeb78b3d2ccfea6a5adc1b6dd61f078e
Paul <paulyk1983@gmail.com>
2016-12-15 20:28:56 -0600

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161208183424) do
 
   # These are extensions that must be enabled in order to support this database
@@ -544,7 +543,7 @@ ActiveRecord::Schema.define(version: 20161208183424) do
     t.string   "last_name"
     t.string   "relationship"
     t.string   "phone_number"
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                                            default: false
     t.string   "family_name"
     t.string   "address"
     t.string   "state"
