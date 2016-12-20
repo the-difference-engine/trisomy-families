57a55d3dfe785158c3097e78cf7035d47f4792e9
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-19 20:53:01 -0600

    Add env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
     bucket: ENV.fetch(ENV['S3_BUCKET_NAME']),
     access_key_id: ENV.fetch(ENV['AWS_ACCESS_KEY_ID']),
     secret_access_key: ENV.fetch(ENV['AWS_SECRET_ACCESS_KEY']),
-    s3_region: ENV.fetch('AWS_REGION'),
+    s3_region: ENV.fetch(ENV['AWS_REGION']),
   }
 }
 
