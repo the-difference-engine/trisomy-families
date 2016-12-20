f31ceb49745307390fe6673424c32ba28f2cd02d
Ramiro Trejo <trejoramiro1@gmail.com>
2016-12-19 20:49:40 -0600

    Modify child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,11 +1,11 @@
 class Child < ApplicationRecord
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", square: '100x100#' }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :trisomy_type, presence: true
   validates :birth_date, presence: true
-  # validates :death_date, presence: true
   validates :state, presence: true
   validates :city, presence: true
   validates :trisomy_story, presence: true
