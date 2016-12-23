9891ec10b4ce0203e4e9bb5aafcddbe8e87ed568
Paul <paulyk1983@gmail.com>
2016-12-22 20:56:30 -0600

    rewrote env credentials for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,10 +98,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch(ENV['S3_BUCKET_NAME']),
-    access_key_id: ENV.fetch(ENV['AWS_ACCESS_KEY_ID']),
-    secret_access_key: ENV.fetch(ENV['AWS_SECRET_ACCESS_KEY']),
-    s3_region: ENV.fetch(ENV['AWS_REGION']),
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_region: ENV['AWS_REGION']
   }
 }
 
