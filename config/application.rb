39224b4642ab1542dd6562ae4759dfe84df057a1
Paul <paulyk1983@gmail.com>
2017-01-10 16:08:42 -0600

    removed manifest file

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,6 @@ module TrisomyFamilies
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.    
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
-
-config.assets.initialize_on_precompile = false
