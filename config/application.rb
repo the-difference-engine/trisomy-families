1c160aa2935b31089ad48404cf71d90176386406
Paul <paulyk1983@gmail.com>
2017-01-10 16:03:31 -0600

    moved code

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module TrisomyFamilies
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
+    # -- all .rb files in that directory are automatically loaded.    
   end
 end
+
+config.assets.initialize_on_precompile = false
