4a8f028cffcf1a6a07dbca890b5f6f7d165366ef
Paul <paulyk1983@gmail.com>
2017-01-11 11:25:22 -0600

    Changed comment to more accurately describe helper

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # Fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
   config.action_controller.include_all_helpers = true
