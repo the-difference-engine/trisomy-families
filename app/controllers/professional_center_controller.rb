eac66d8d9936dc9b253c9777ea8512176b438081
matvacha <matvacha@yahoo.com>
2017-01-16 21:01:46 -0600

    get unirest working with api

--- app/controllers/professional_center_controller.rb
+++ app/controllers/professional_center_controller.rb
@@ -1,6 +1,6 @@
 class ProfessionalCenterController < ApplicationController
   def index
-    children = Unirest.get('http://localhost:3000/api/v1/children.json')
+    @children = Unirest.get('http://localhost:3000/api/v1/children.json').body
     render 'index.html.erb'
   end
 end
