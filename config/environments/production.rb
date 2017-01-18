920ec5ed7185d19b4eb5d1f9f0d5c2d0d2a8b53d
Paul <paulyk1983@gmail.com>
2017-01-18 13:44:02 -0600

    testing...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
 
-  # Fallback to assets pipeline if a precompiled asset is missed.
+  # Fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   config.action_controller.include_all_helpers = true
