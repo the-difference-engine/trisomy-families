24b60badeca31d7b23cfbdc97f0f826a7f8c5136
matvacha <matvacha@yahoo.com>
2017-01-19 20:40:36 -0600

    add characteristics checkboxes and state list, still need to make lists for characteristics

--- app/assets/javascripts/queriesCtrl.js
+++ app/assets/javascripts/queriesCtrl.js
@@ -1,34 +1,15 @@
 /* global angular */
 angular.module("app").controller('queriesCtrl', function($scope, $http) {
- 
-//Define the `myDataSource` scope variable.
-  var children = $http.get('/api/v1/children.json');
-  $scope.myDataSource = {
-    chart: {
-      caption: "Trisomy Tables",
-      numberPrefix: "$",
-      theme: "fint"
-    },
+  $scope.setup = function() {
+    $http.get('/api/v1/children.json').body;
+  };
 
-    data:[{
-      label: children['state'],
-      value: "880000"
-    },
-    {
-      label: "Garden Groove harbour",
-      value: "730000"
-    },
-    {
-      label: "Los Angeles Topanga",
-      value: "590000"
-    },
-    {
-      label: "Compton-Rancho Dom",
-      value: "520000"
-    },
-    {
-      label: "Daly City Serramonte",
-      value: "330000"
-    }]
+  $scope.createQuery = function(state, treatment, surgery, characteristic) {
+    var params = {
+      state: state,
+      treatment: treatment,
+      surgery: surgery,
+      characteristic: characteristic
+    };
   };
 });
