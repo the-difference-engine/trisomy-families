6429dd494d49d98cb44b16a3b399c47884751ddb
matvacha <matvacha@yahoo.com>
2017-01-23 20:56:40 -0600

    add flash warning

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,7 @@ class AdminsController < ApplicationController
     if current_user.user_type == 'admin'
       render 'index.html.erb'
     else
+      flash[:warning] = 'You must be an administrator to view this page.'
       redirect_to '/'
     end
   end
