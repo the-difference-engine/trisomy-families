489a806036169e03884d547997da3687930019fa
matvacha <matvacha@yahoo.com>
2017-02-01 14:55:21 -0600

    remove angular flash alert --- add at a later time

--- app/assets/javascripts/adminCtrl.js
+++ app/assets/javascripts/adminCtrl.js
@@ -1,30 +1,31 @@
 /* global angular */
 (function() {
-  angular.module('app').controller('adminCtrl', function($scope, $http) {
-    $http.get('api/v1/users.json').then(function(response) {
-      $scope.users = response.data;
-    });
+  angular.module('app').controller('adminCtrl',
+    function($scope, $http) {
+      $http.get('api/v1/users.json').then(function(response) {
+        $scope.users = response.data;
+      });
 
-    $scope.setOrderAttribute = function(inputAttribute) {
-      if (inputAttribute !== $scope.orderAttribute) {
-        $scope.isOrderDescending = false;
-      } else {
-        $scope.isOrderDescending = !$scope.isOrderDescending;
-      }
-      $scope.orderAttribute = inputAttribute;
-    };
-
-    $scope.updateType = function(type, id) {
-      var params = {
-        id: id,
-        user_type: type
+      $scope.setOrderAttribute = function(inputAttribute) {
+        if (inputAttribute !== $scope.orderAttribute) {
+          $scope.isOrderDescending = false;
+        } else {
+          $scope.isOrderDescending = !$scope.isOrderDescending;
+        }
+        $scope.orderAttribute = inputAttribute;
       };
 
-      $http.patch('api/v1/users.json', params).then(function(response) {
-        $scope.users.push(response.data);
-      });
-    };
+      $scope.updateType = function(type, id) {
+        var params = {
+          id: id,
+          user_type: type
+        };
 
-    window.$scope = $scope;
-  });
+        $http.patch('api/v1/users.json', params).then(function(response) {
+          $scope.users.push(response.data);
+        });
+      };
+
+      window.$scope = $scope;
+    });
 })();
