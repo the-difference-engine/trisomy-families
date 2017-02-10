16daf1fbd416bc6710348bc7cd4d951eb6a3b50e
matvacha <matvacha@yahoo.com>
2017-02-09 20:36:51 -0600

    change app controller before method to fix deprecation, comment out home method in users controller to redirect to home page, write and change corresponding tests

--- spec/features/users/sign_in_spec.rb
+++ spec/features/users/sign_in_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature "User signs in", :type => :feature do
     fill_in "user_password", :with => user.password
     find('input[name="commit"]').click
 
-    expect(page.current_path).to eq "/admin-dashboard"
+    expect(page.current_path).to eq "/"
   end
 
   scenario "User is not an admin" do 
