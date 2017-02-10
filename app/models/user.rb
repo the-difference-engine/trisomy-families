fff1602bb6ff8e889a4600b1cbd7e0ba3c15afaf
nachomacaso <nathanmacaso@gmail.com>
2017-02-09 20:06:57 -0600

    Added family and doctor radio buttons to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,9 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :relationship, presence: true, on: :update
-<<<<<<< HEAD
   validates :phone_number, presence: true, on: :update
   validates :user_type, presence: true, on: :update
-=======
   validates :phone_number, presence: true
   validates :city, presence: true
   validates :state, presence: true
->>>>>>> 72a56af17d8e62201dc94ab08523c3679d4f2e15
 end
