db2a09acae602428ba9dad0e41c43374a7ccc1da
nachomacaso <nathanmacaso@gmail.com>
2017-02-13 20:11:33 -0600

    sign-up redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :city, :state, :phone_number, :user_type, :email, :password) }
   end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if current_user.user_type == "doctor"
+      "/profile_doctor"
+    elsif current_user.user_type == "family"
+      "/profile"
+    end
+  end
 end
