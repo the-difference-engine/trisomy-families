eb89c6afff3e3f2bed3a33f0789b81cc1083041d
justin skord <jskord@gmail.com>
2017-02-16 20:27:59 -0600

    added address field to user login

--- app/controllers/physicians_controller.rb
+++ app/controllers/physicians_controller.rb
@@ -1,6 +1,6 @@
 class PhysiciansController < ApplicationController
   def new_profile
-    @physician = Physician.new()
+    @physician = Physician.new
     render 'physician_create_profile_page.html.erb'
   end
 
@@ -30,5 +30,37 @@ class PhysiciansController < ApplicationController
   def show
     @physician = Physician.find_by(id: params[:id])
     render 'show.html.erb'
+  end
+
+  def edit
+    if current_user.user_type == 'doctor' || current_user.user_type == 'admin'
+      @physician = Physician.find_by(id: params[:id])
+      render 'edit.html.erb'
+    else
+      flash[:warning] = 'You must be a doctor to edit this page!'
+      redirect_to '/'
+    end
+  end
+
+  def update
+    @physician = Physician.find_by(id: params[:id])
+
+    @physician.update(
+      first_name: params[:first_name] || @physician.first_name,
+      last_name: params[:last_name] || @physician.last_name,
+      phone_number: params[:phone_number] || @physician.phone_number,
+      address: params[:address] || @physician.address,
+      state: params[:state] || @physician.state,
+      city: params[:city] || @physician.city,
+      zip_code: params[:zip_code] || @physician.zip_code,
+      website: params[:website] || @physician.website,
+      speciality: params[:speciality] || @physician.speciality
+    )
+    if @physician.save
+      flash[:success] = 'Profile Updated!'
+    else
+      flash[:warning] = 'Error!'
+    end
+    redirect_to "/profile_doctor/#{@physician.id}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :relationship, presence: true, on: :update
   validates :phone_number, presence: true, on: :update
-  validates :user_type, presence: true, on: :update
+  validates :user_type, presence: true
   validates :phone_number, presence: true
   validates :city, presence: true
   validates :state, presence: true
