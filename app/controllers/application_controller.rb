c1d33e773a2ff32a59157069ec343765b55921fd
Ramiro Trejo <trejoramiro1@gmail.com>
2017-02-26 19:31:16 -0600

    Add layout based on logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout :logged_in?
   protect_from_forgery with: :null_session
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
@@ -7,5 +8,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :city, :state, :phone_number, :email, :password) }
+  end
+
+  private
+  def logged_in?
+    if user_signed_in?
+      "application"
+    else
+      "fullscreen"
+    end
   end
 end
