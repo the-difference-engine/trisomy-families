976139a441c48c3d135438b8a44837b567ce3faa
justin skord <jskord@gmail.com>
2017-03-04 22:50:25 -0600

    added associations with family and children databses

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,5 @@
 class Child < ApplicationRecord
+  belongs_to :family
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", square: '100x100#' }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
--- app/models/family.rb
+++ app/models/family.rb
@@ -1,2 +1,3 @@
 class Family < ApplicationRecord
+  has_many :children
 end
