37fd38fddd378684a1a34572614b92b091e6ac3c
justin skord <jskord@gmail.com>
2017-03-06 16:28:51 -0600

    got geocoder workingf or family database

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,13 @@
 class Family < ApplicationRecord
   has_many :children
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    [street_address, city, state].compact.join(', ')
+  end
+
+  def hello
+    puts "hello! I'm a family!"
+  end
 end
