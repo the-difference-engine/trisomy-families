ddb9080f698a1cadf1b2a22fb1015d82ed444e69
justin skord <jskord@gmail.com>
2017-03-06 19:22:10 -0600

    added family model test

--- spec/models/family_spec.rb
+++ spec/models/family_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Family, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Creates a valid Family" do
+    family = Family.new(
+      family_name: "Smith",
+      street_address:  "1871 Merch Mart",
+      city: "Chicago",
+      state: "Illinois",
+      latitude: 47.0,
+      longitude: 100.0
+    )
+    family.save
+
+    it 'is valid on save' do
+      expect(family).to be_valid
+    end
+  end
 end
