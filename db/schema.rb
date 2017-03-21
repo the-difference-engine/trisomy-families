5e4b60721b4069d790f1a4becbb557afc75e93b1
Paul <paulyk1983@gmail.com>
2017-03-20 20:33:24 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170317035256) do
+ActiveRecord::Schema.define(version: 20170317020245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -487,7 +487,7 @@ ActiveRecord::Schema.define(version: 20170317035256) do
     t.string   "state"
     t.string   "zip_code"
     t.string   "website"
-    t.string   "specialty"
+    t.string   "speciality"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.decimal  "latitude",             precision: 13, scale: 10
