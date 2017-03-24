750f5cfe47114abf9134cb674b86cd8be2fd7caa
justin skord <jskord@gmail.com>
2017-03-23 19:54:43 -0500

    added family photo to edit page

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -70,10 +70,20 @@ class FamiliesController < ApplicationController
   def update
     @family = Family.find_by(id: params[:id])
 
-    @family.family_name = params[:family_name]
-    @family.story = params[:story]
-    @family.city = params[:city]
-    @family.state = params[:state]
+    @family.family_name = params[:family][:family_name]
+    @family.story = params[:family][:story]
+    @family.city = params[:family][:city]
+    @family.state = params[:family][:state]
+
+    charset = Array('A'..'Z') + Array('a'..'z')
+    random_string = Array.new(20) { charset.sample }.join
+    object_name = random_string + '_' + params["family"]["photo"].original_filename
+    obj = S3_BUCKET.object(object_name)
+    obj.upload_file(params["family"]["photo"].tempfile.path)
+    puts "********"
+    puts @family
+    @family.update(photo: obj.public_url)
+    @family.save
 
     @family.save
 
