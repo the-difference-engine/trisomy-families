9046ae196bdc056f573fd42b912a6c0fc9ab2330
Russell Mstowski <rmstow2@uic.edu>
2017-04-06 20:25:22 -0500

    add redirect logic and spec for physicians

--- spec/controllers/physicians_controller_spec.rb
+++ spec/controllers/physicians_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PhysiciansController, type: :controller do
+  describe '#show' do
+    before(:example) {
+      user = FactoryGirl.create(:user)
+      physician = FactoryGirl.create(:physician)
+      physician.user_id = user.id
+      sign_in user
+      get(:show, params: {id: user.id })
+    }
 
+    it 'renders the physicians show page' do
+      expect(response).to render_template('show.html.erb')
+    end
+  end
 end
