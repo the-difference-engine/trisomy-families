7ef930edc96aff6ffc5fb238f936c2003f2590c6
millarba <millarba@gmail.com>
2017-05-04 19:24:01 -0500

    made family story resizable on family edit

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,4 +1,4 @@
-class ChildrenController < ApplicationController
+hclass ChildrenController < ApplicationController
   def new
     @family = Family.find_by(id: current_user.family_id)
     unless user_signed_in?
