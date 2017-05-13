bc8d51601e2e92a418e10434b6dc617491772501
justin skord <jskord@gmail.com>
2017-05-13 18:57:12 -0500

    reset authentication for professional center

--- app/controllers/professional_center_controller.rb
+++ app/controllers/professional_center_controller.rb
@@ -3,7 +3,7 @@ class ProfessionalCenterController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.user_type == 'admin' || current_user.user_type == 'doctor'
+    if current_user.user_type == 'doctor'
       render 'index.html.erb'
     else
       flash[:error] = 'You do not have access to this page. Please contact an admin.'
