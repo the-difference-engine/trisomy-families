afb45d1b82cf3061a8ecb4255a99fe042f05c0e1
justin skord <jskord@gmail.com>
2017-05-13 18:58:43 -0500

    changed error message

--- app/controllers/professional_center_controller.rb
+++ app/controllers/professional_center_controller.rb
@@ -6,7 +6,7 @@ class ProfessionalCenterController < ApplicationController
     if current_user.user_type == 'doctor'
       render 'index.html.erb'
     else
-      flash[:error] = 'You do not have access to this page. Please contact an admin.'
+      flash[:error] = 'You must be a doctor to use the Professional Center tool'
       redirect_to root_url
     end
   end
