8dc19b4e87ee5ef310d556d845ec8259361444dd
justin skord <jskord@gmail.com>
2017-05-16 00:25:42 -0500

    still tinkering with piechart

--- app/assets/javascripts/trisomy_type_bar_chart.js
+++ app/assets/javascripts/trisomy_type_bar_chart.js
@@ -4,6 +4,8 @@ var trisomy_type_hash_keys = [];
 var trisomy_type_hash_values = [];
 var birthdate = [];
 var age = [];
+var piechart_data = [];
+
  
 $.getJSON('/api/v1/children', function(data) {
     data.forEach(function(child) {
@@ -88,6 +90,64 @@ Highcharts.chart('container', {
         data: trisomy_type_hash_values
     }]
 });
+});
+
+$(function () { 
+Highcharts.chart('container2', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: ''
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Microsoft Internet Explorer',
+            y: 56.33
+        }, {
+            name: 'Chrome',
+            y: 24.03,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Firefox',
+            y: 10.38
+        }, {
+            name: 'Safari',
+            y: 4.77
+        }, {
+            name: 'Opera',
+            y: 0.91
+        }, {
+            name: 'Proprietary or Undetectable',
+            y: 0.2
+        }]
+    }]
 }); 
 }); 
+});
+console.log(trisomy_type_hash)
+var data = JSON.stringify(trisomy_type_hash);
+console.log(data);
 
