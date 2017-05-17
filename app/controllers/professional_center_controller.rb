7273ec63028125897cbc93ad2316457f7a31064e
Paul <paulyk1983@gmail.com>
2017-05-17 15:47:30 -0500

    added admin to professional center permissions

--- app/controllers/professional_center_controller.rb
+++ app/controllers/professional_center_controller.rb
@@ -3,7 +3,7 @@ class ProfessionalCenterController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.user_type == 'doctor'
+    if current_user.user_type == 'doctor' || current_user.user_type == "admin"
       render 'index.html.erb'
     else
       flash[:error] = 'You must be a doctor to use the Professional Center tool'
