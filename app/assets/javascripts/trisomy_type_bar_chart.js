c25a513f3f318e7f18468ce25adffd27e1db5518
justin skord <jskord@gmail.com>
2017-05-18 15:40:21 -0500

    moved piechart to separate file

--- app/assets/javascripts/trisomy_type_bar_chart.js
+++ app/assets/javascripts/trisomy_type_bar_chart.js
@@ -93,59 +93,6 @@ Highcharts.chart('container', {
 });
 });
 
-var trisomy_types_piechart_keys = [];
-var trisomy_types_values = []
-
-// for (i = 0; i < 5; i++) {
-//     text += "The number is " + i + "<br>";
-// }
-
-$.getJSON('/api/v1/children/trisomy_type_totals', function(data) {
-    var json = data; 
-
-    Highcharts.chart('container2', {
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-        },
-        title: {
-            text: ''
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                }
-            }
-        },
-
-
-    series: 
-
-    [{
-        name: 'Trisomy Type of Registered Children',
-        colorByPoint: true,
-        data: json
-
-
-
-
-
-    }]
-}); 
-}); 
-
 
 
 
