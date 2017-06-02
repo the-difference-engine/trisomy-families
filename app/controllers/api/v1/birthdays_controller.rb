ccc9043c64924f08a46c7f7db6005299eacae0be
Paul <paulyk1983@gmail.com>
2017-06-01 19:08:37 -0500

    added logic to api to only display children who have designated day and month in their birthday field

--- app/controllers/api/v1/birthdays_controller.rb
+++ app/controllers/api/v1/birthdays_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::BirthdaysController < ApplicationController
 
   def index
-    @children = Child.all
+    @children = Child.where('extract(month from birth_date) = ? AND extract(day from birth_date) = ?', '01', '01')
     render 'index.json.jbuilder'
   end
 
