4a43cf8b0eebca38c1921c185e0f2d1a18362f84
millarba <millarba@gmail.com>
2017-06-05 18:52:06 -0500

    added conditional to require a logged in user to view family center

--- app/controllers/family_center_controller.rb
+++ app/controllers/family_center_controller.rb
@@ -1,4 +1,9 @@
 class FamilyCenterController < ApplicationController
   def index
+    if !current_user
+      flash[:warning] = 'You must be logged in to view this page.'
+
+      redirect_to '/users/sign_in'
+    end
   end
 end
