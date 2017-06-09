2438b0b3100e6a2b0433590f891d890bd4368abf
Paul <paulyk1983@gmail.com>
2017-06-09 17:19:05 -0500

    added button enable disable dynamic and fixed small redirect bug in bgroundhistory controller

--- app/controllers/background_history_forms_controller.rb
+++ app/controllers/background_history_forms_controller.rb
@@ -121,7 +121,7 @@ class BackgroundHistoryFormsController < ApplicationController
          background_history_id: @background_history.id
        )
       flash[:success] = "Background History Successfully added!"
-      redirect_to "/children/#{@child.id}"
+      redirect_to "/profile/#{@child.id}"
     else
       flash[:warning] = "Unable to add Background History!"
       render 'new.html.erb'
