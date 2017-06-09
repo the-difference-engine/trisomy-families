05c82b8f0de04e90cf6245bc749723444f92ca93
Paul <paulyk1983@gmail.com>
2017-06-09 17:33:52 -0500

    changed redirect after submitting contact info

--- app/controllers/contact_info_forms_controller.rb
+++ app/controllers/contact_info_forms_controller.rb
@@ -14,10 +14,10 @@ class ContactInfoFormsController < ApplicationController
     family = Family.find_by(user_id: current_user.id)
     contact_info = ContactInfoForm.new(contact_info_params)
     contact_info.update(child_id: params[:child_id])
-    
+
     if contact_info.save      
       flash[:success] = "Contact info for your child registration has been submitted!"
-      redirect_to "/families/#{family.id}"
+      redirect_to "/profile/#{params[:child_id]}"
     else
       flash[:danger] = "Something went wrong. Please fill out every field in the form."
       render 'new.html.erb'
@@ -33,11 +33,11 @@ class ContactInfoFormsController < ApplicationController
 
   def update
     family = Family.find_by(user_id: current_user.id)
-    contact_info = ContactInfoForm.new
+    contact_info = ContactInfoForm.find_by(id: params[:id])
 
     if contact_info.update(contact_info_params)
       flash[:success] = "Contact info for your child registration has been update!"
-      redirect_to "/families/#{family.id}"
+      redirect_to "/profile/#{contact_info.child_id}"
     else
       flash[:danger] = "Something went wrong. Please fill out every field in the form."
       render 'edit.html.erb'
