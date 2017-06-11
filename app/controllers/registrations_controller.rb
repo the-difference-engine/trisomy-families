ccb13e2f17aaeeb1943abf707e260800a5a59049
Paul <paulyk1983@gmail.com>
2017-06-11 15:00:48 -0500

    added registration show page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,17 @@ class RegistrationsController < ApplicationController
   end
 
   def show
-    @child = Child.find(params[:id])
-    @contact_info = ContactInfoForm.find_by(child_id: @child.id)
-    @background_histroy = BackgroundHistory.find(@child.background_histroy_id)
-    @health_history = HealhHistory.find(@child.health_history_id)
+    @child = Child.find_by(id: params[:id])
+    if @child
+      @family = Family.find_by(id: @child.family_id)
+      @contact_info = ContactInfoForm.find_by(child_id: @child.id)
+      @background_history = BackgroundHistory.find_by(id: @child.background_history_id)
+      @health_history = HealthHistory.find_by(id: @child.health_history_id)
+      render 'show.html.erb'
+    else
+      flash[:danger] = "That registration does not exist! Do have a child/participant profile?"
+      redirect_to '/'
+    end
   end
 
   def destroy
